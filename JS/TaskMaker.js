e3f1a908b016db3e13ac8fc6905e931bc9495b38
Vedantttgitteee20 <83120606+Vedantttgitteee20@users.noreply.github.com>
2021-08-07 02:31:56 +0530

    -TaskMaker Added

---
+++ JS/TaskMaker.js
@@ -0,0 +1,64 @@
+console.log("hi");
+const inputBox = document.querySelector(".inputField  input");
+const addBtn =document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const deleteAllBtn= document.querySelector(".TaskFooter button");
+inputBox.onkeyup =()=>{
+    let userData =inputBox.value;
+    if(userData.trim() != 0){
+        addBtn.classList.add("active");
+    }
+    else{
+        addBtn.classList.remove("active");
+    }
+}
+showTasks();
+addBtn.onclick = () =>{
+    let userData=inputBox.value;
+    let getLocalStorage = localStorage.getItem("new Todo");
+    if(getLocalStorage==null){
+        listArr =[];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userData);
+    localStorage.setItem("new Todo",JSON.stringify(listArr));
+    showTasks();
+}
+
+function showTasks(){
+    let getLocalStorage = localStorage.getItem("new Todo");
+    if(getLocalStorage==null){
+        listArr =[];
+    }else{
+        listArr = JSON.parse(getLocalStorage);
+    }
+    const pendingnum =document.querySelector(".pendingnum");
+    pendingnum.textContent = listArr.length;
+    if(listArr.length>0)
+    {
+        deleteAllBtn.classList.add("active");
+    }
+    else{
+        deleteAllBtn.classList.remove("active");
+    }
+    let newLiTag= '';
+    listArr.forEach((element,index) => {
+        newLiTag =newLiTag + `<li>${element}<span onClick = "deleteTask(${index})";><i class="fas fa-trash"></i></span></li>`
+    });
+    todoList.innerHTML = newLiTag;
+    inputBox.value="";
+}
+
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("new Todo");
+    listArr = JSON.parse(getLocalStorage);
+    listArr.splice(index, 1);
+    localStorage.setItem("new Todo",JSON.stringify(listArr));
+    showTasks();
+}
+deleteAllBtn.onclick = ()=>{
+    listArr=[];
+    localStorage.setItem("new Todo", JSON.stringify(listArr));
+    showTasks();
+}
